cc414089d35ee81b26c9698fd6b2d53d4babcbd9
hayat <lahlalihayat70@gmail.com>
2025-10-14 13:53:59 +0200

    bonjour

---
+++ challenge1.js
@@ -0,0 +1,38 @@
+const prompt = require("prompt-sync")();
+/*let Nom = prompt("entrer un nom :");
+let Prenom = prompt ("entrer un prenom :");
+let sexe = prompt ("entrer un sexe :");
+let numero = prompt ("entrer un numero de telephone:");
+console.log("le nom est:"+Nom,"le prenom est:"+Prenom,"sexe est:"+ sexe,"le numero de telephone est:"+numero);*/
+
+
+/*let A = 4;
+let B = 5;
+let C = 7;
+
+A = B;
+B = C;
+C = A;
+console.log("A="+A, "B="+B, "C="+C);*/
+
+
+let a = parseFloat(prompt("entrer un nomber"));
+let b = parseFloat( prompt("entrer un nombre"));
+let somme = a+b;
+let sostr = a-b;
+let mult = a*b;
+let reste = a%b;
+console.log("la somme est:"+somme);
+console.log("la sostraction est:"+sostr);
+console.log("la multiplication est:"+mult);
+console.log("la reste est:"+reste);
+
+
+
+
+
+
+
+
+
+
