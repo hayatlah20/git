347d607d2c54522420a5a00e358eff74e4362804
hayat <lahlalihayat70@gmail.com>
2025-10-15 18:49:06 +0200

    hey

--- challenge3.js
+++ challenge3.js
@@ -2,12 +2,12 @@ const prompt = require("prompt-sync")();
 
 /*exercice 1*/
 
-let nbr = parseInt(prompt("entrer un nomber:"));
+/*let nbr = parseInt(prompt("entrer un nomber:"));
 console.log("la table de multiplication est:");
 for(let i=1;i<=10;i++){
     M = nbr*i; 
     console.log(nbr + "x" + i + "=" + M);
-}
+}*/
 
 /*exercice 2*/
 
---
+++ challenge4.js
@@ -0,0 +1,37 @@
+ /*exercice 5*/
+
+/*let tableux = [ 1,4,9,7,5,8,2];
+let max = 0;
+let min = 0;
+for(i = 1;i<= tableux.length;i++){
+    if(tableux[i]>max){
+        max=tableux[i];
+    }
+    if(tableux[i]<min){
+        min=tableux[i];
+    }
+ }
+ console.log("le nomber plus grand est:"+max);
+ console.log("le nomber plus petit est:"+min);*/
+
+ /*exercice2*/
+
+ /*let arr =[1,2,3,4,5,6,7,8,9,10];
+ let somme = 0;
+ for(let i=0;i<arr.length;i++){
+    somme=somme + arr[i];
+ }
+ moyenne = somme/arr.length;
+ console.log("la somme est :"+somme);
+ console.log("la moyenne est :"+moyenne);*/
+
+ /*exercice6*/
+
+ let tab =[2,10,4,5,9];
+ for(let i=0;i<tab.length;i++){
+    C = tab[i] * tab[i];
+ }
+ console.log("carre est:"+C);
+
+
+
