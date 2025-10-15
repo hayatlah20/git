68a733a43077503c122e80a35e99c7a4c43b399e
hayat <lahlalihayat70@gmail.com>
2025-10-15 13:33:26 +0200

    salut

---
+++ challenge3.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+
+/*exercice 1*/
+
+let nbr = parseInt(prompt("entrer un nomber:"));
+console.log("la table de multiplication est:");
+for(let i=1;i<=10;i++){
+    M = nbr*i; 
+    console.log(nbr + "x" + i + "=" + M);
+}
+
+/*exercice 2*/
+
